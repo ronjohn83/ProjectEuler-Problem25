ae1bc2d6b2acf8bbdecf17c5adc92be9cd0c4bab
Ronald Johnson <ronjohn83@hotmail.com>
2017-04-12 11:08:33 -0400

    Initial Commit

---
+++ Problem 25/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ThousandDigitFibonacci());
+            Console.Read();
+        }
+
+        private static int ThousandDigitFibonacci()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            int index = 0;
+            BigInteger previous = 0;
+            BigInteger prePrevious = 0;
+            BigInteger fibonacci = 1;
+            BigInteger fibSum = 0;
+
+            while(fibSum.ToString().Length < 1000)
+            {
+                index++;
+
+                fibonacci += previous + prePrevious;
+
+                prePrevious = previous;
+                previous = fibonacci;
+                fibSum = fibonacci;
+                fibonacci = 0;
+                
+            }
+
+            stopwatch.Stop();
+            return index;
+        }
+    }
+}
